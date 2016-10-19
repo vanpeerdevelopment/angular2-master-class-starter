d4c16ddc5c51b09f14fd461c536a049cbbf4639b
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2016-10-19 11:20:08 +0200

    exercise 2 - jump start - display first contact

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Contact} from "./models/contact";
 
 @Component({
   selector: 'trm-contacts-app',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
+  contact:Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/7.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  };
 }
